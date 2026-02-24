b1453efd4173dd01c95dc23f0ca13cea2697a3f9
Alex Storm <stormeal@gmail.com>
2026-02-24 10:32:21 +0100

    removing solutions

--- tests/solutions/exercise1/exercise_1.solution.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("Exercise 1: open site and take screenshot", { tag: "@ex1" }, async ({ page }) => {
-  await page.goto("https://testhuset.dk", { waitUntil: "domcontentloaded" });
-  await page.screenshot({ path: "screenshots/day1_exercise1.png", fullPage: true });
-});
